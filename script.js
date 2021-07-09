ac212fe74195761336923013384943d229512832
FrancoPagnotta <francoapagnotta@gmail.com>
2021-07-09 02:47:50 -0300

    console errors about go up button resolved

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
+// NAV BAR
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__list__item--link');
-const goUp = document.querySelector('.social-list__link__go-up');
-const oldElement = document.querySelector('.old-element')
-const newElement = document.querySelector('.new-element');
 
-
-// NAV BAR
 navToggle.addEventListener('click', (e) => {
     document.body.classList.toggle('nav-open');
 }); 
-
 
 navLinks.forEach(link => {
     link.addEventListener('click', (e)=> {
@@ -18,19 +13,26 @@ navLinks.forEach(link => {
 });
 
 
+
 // GO UP BUTTON
+const goUp = document.querySelector('.social-list__link__go-up');
+const primerHijo = document.querySelector('.primerHijo');
+
+const segundoHijo = document.createElement("A");
+segundoHijo.classList.add("social-list__link__go-up");
+segundoHijo.setAttribute("href","#header");
+segundoHijo.innerHTML = "up!";
+
 goUp.addEventListener('mouseover', (e)=> {
-    
-    newElement.innerHTML = 'up!';
-    newElement.classList.add('go-up')
-    goUp.replaceChild(newElement,oldElement);
-    goUp.removeChild(oldElement);
+    primerHijo.classList.remove("fas");
+    primerHijo.classList.remove("fa-arrow-circle-up");
+    primerHijo.classList.remove("primerHijo");
+    goUp.appendChild(segundoHijo)
 });
 
-
 goUp.addEventListener('mouseout', (e)=> {
-    newElement.innerHTML = '';
-    goUp.replaceChild(oldElement,newElement);
-    goUp.removeChild(newElement);
-    
+    goUp.removeChild(segundoHijo);
+    primerHijo.classList.add("fas");
+    primerHijo.classList.add("fa-arrow-circle-up");
+    primerHijo.classList.add("primerHijo");
 });
